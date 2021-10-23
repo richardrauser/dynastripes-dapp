a688bb01adf804c502cc12e988a9e6d904adf6f9
Richard Rauser <richard@raaza.net>
2021-10-23 10:49:44 +0100

    how to updates

--- src/pages/HowToPage.js
+++ src/pages/HowToPage.js
@@ -25,20 +25,36 @@ class HowToPage extends React.Component {
                     Metamask is the leading Ethereum wallet for browsing the decentralised web.
                   <ol>
                     <li>Visit <MetaMaskLink /> and download the MetaMask wallet. It will be a <a className="externalLink" href="https://en.wikipedia.org/wiki/Browser_extension" target="_blank" rel="noreferrer">browser extension</a> if you're accessing this from a desktop computer, or a mobile app if using a smartphone. The MetaMask website should detect this. Most people will find it easier from a desktop browser rather than mobile. The mobile experience of the decentralised web is still a bit crude!</li> 
-                    <li>Unfortnately MetaMask is not supported for Safari on MacOS. 😢 Try the <a className="externalLink" href="https://www.google.com/chrome/Chrome" target="_blank" rel="noreferrer">Chrome</a> browser. You'll love it. It's quick and easy to install.</li>
+                    <li>Unfortunately MetaMask is not supported for Safari on MacOS. 😢 Try the <a className="externalLink" href="https://www.google.com/chrome/Chrome" target="_blank" rel="noreferrer">Chrome</a> browser. You'll love it. It's quick and easy to install.</li>
                     <li>Once installed, open MetaMask. On Chrome, it'll appear as a little fox icon in your extensions toolbar up by the URL bar. </li>
                     <li>Create a new account by following the instructions in MetaMask.</li> 
-                    <li>Connect your wallet to the Rinkeby network by tapping the text at top that says "Ethereum Main Network" and then selecting "Rinkeby Test Network". Rinkeby is a test network where you will not be spending real funds.</li>
+                    <li>Connect your wallet to the Rinkeby network. Rinkeby is a test network where you will not be spending real funds.</li>
+                      <ul>
+                        <li>On desktop, tap the "Ethereum Main Network" text at the top of the MetaMask window and select "Rinkeby Test Network". </li>
+                      </ul>    
+                      <ul>
+                        <li>On mobile, tap the top left menu button in the MetaMask app and select "Wallet". Then tap the "Ethereum Main Network" text at the top and select "Rinkeby Test Network". </li>
+                      </ul>    
+            
+                      
+                      
                   </ol>
 
                   <h3>Acquire some ETH
                   </h3>
-                  <p> The Rinkeby network is a test network and so you can't spend real money on ETH. You can, however, use a "faucet" to pay out some ETH to your wallet!
+                  <p> The Rinkeby network is a test network and so you can't spend real money on ETH. You can, however, use a "faucet" to pay out some ETH to your wallet for free.
                   </p>
                   <ol>
-                    <li>Ensure you've selected Rinkeby as your network in MetaMask as per the 3rd step above.</li> 
-                    <li>Visit <a className="externalLink" href="https://app.mycrypto.com/faucet" target="_blank" rel="noreferrer">MyCrypto faucet</a>.</li>
-                    <li>Select your Rinkeby account in the Recipient dropdown. MyCrypto should detect your account. Hit the Request Assets button.</li> 
+                    <li>Ensure you've selected Rinkeby as your network in MetaMask as per the final step above.</li> 
+                    <li>Visit an ETH faucet site and request some ETH:</li>
+                    <ul>
+                        <li>In a desktop browser, try <a className="externalLink" href="https://app.mycrypto.com/faucet" target="_blank" rel="noreferrer">MyCrypto faucet</a>.</li>
+                      </ul>    
+                      <ul>
+                        <li>In the MetaMask mobile app, try <a className="externalLink" href="https://faucet.metamask.io/" target="_blank" rel="noreferrer">MetaMask faucet</a>. </li>
+                      </ul>    
+                       
+                    <li>Follow the steps for whatever faucet you chose. </li> 
                     <li>A <a className="externalLink" href="https://en.wikipedia.org/wiki/CAPTCHA" target="_blank" rel="noreferrer">CATCHPA</a> will appear. Enter it and hit submit.</li>
                     <li>Congratulations! you now have some ETH in your wallet. It may take a second to appear. Open your MetaMask wallet and your ETH balance will eventually update.</li>
                   </ol>
@@ -55,7 +71,8 @@ class HowToPage extends React.Component {
                   You're ready to start having some fun! 😁
                   
                   <ol>
-                    <li>Return to this website in your browser. If on mobile, you'll need to use the browser in the MetaMask app.</li>
+                    <li>Return to this website in your browser. If on mobile, you'll 
+                      need to use the browser in the MetaMask app.</li>
                     <li>Hit the Connect Wallet button at the top right of <DynaSpan/>. If you don't see it or an Install MetaMask button apearrs, refresh your browser.</li>
                     <li>Follow the MetaMask prompts to connect your ETH wallet to <DynaSpan/>.</li>
                     <li>Browse to the <Link to="/mint">Mint page</Link> to create a new NFT!</li> 
