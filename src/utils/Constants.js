5b11a087188c7876f432a7d1253cc83338459bc2
Richard Rauser <richard@raaza.net>
2021-11-10 11:33:34 +0000

    updating for rinkeby

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,9 +1,9 @@
 
-const DynaStripesCurrentEthNeworkID = 1337; // HardHat localhost
-const DynaStripesContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; // localhost
+// const DynaStripesCurrentEthNeworkID = 1337; // HardHat localhost
+// const DynaStripesContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; // localhost
 
-// const DynaStripesCurrentEthNeworkID = 4; // rinkeby
-// const DynaStripesContractAddress = '0x3a57FbD471E226a5aD9003b44D9A4567AeE4674F'; // rinkeby
+const DynaStripesCurrentEthNeworkID = 4; // rinkeby
+const DynaStripesContractAddress = '0x3a57FbD471E226a5aD9003b44D9A4567AeE4674F'; // rinkeby
 
 // const DynaStripesCurrentEthNeworkID = 3; // ropsten
 // const DynaStripesContractAddress = '0x379A3dA759A131504085E485a75cA2202fB80476'; // ropsten
