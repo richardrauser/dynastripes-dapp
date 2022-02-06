0712fa336b07f39b4ce06d9364edc1ad7e7c829e
Richard Rauser <richard@raaza.net>
2022-02-06 14:52:33 +0000

    pointing at mumbai with new contract address

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,29 +1,31 @@
 
 // localhost / Hardhat
 // const DynaStripesCurrentNeworkID = 1337; 
-// const DynaStripesCurreNetworkName = "localhost";
+// const DynaStripesCurrentNetworkName = "localhost";
+// const DynaStripesCurrentNetworkCurrencySymbol = "MATIC";
+// const DynaStripesCurrentNetworkRpcUrl = "https://www.superbad.com/";
+// const DynaStripesCurrentNetworkExplorerUrl = 'https://www.superbad.com/';
 // const DynaStripesContractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; 
-// const DynaStripesEtherscanLink = 'www.superbad.com';
 
 // ETHEREUM
 
 // Rinkeby
 // const DynaStripesCurrentNeworkID = 4; 
-// const DynaStripesCurreNetworkName = "Rinkeby Testnet";
+// const DynaStripesCurrentNetworkName = "Rinkeby Testnet";
 // const DynaStripesContractAddress = '0x95C2274aCC5B0AEc7d1544d71CF4620124f6ec1c';
-// const DynaStripesEtherscanTxLink = 'https://rinkeby.etherscan.io/'; 
+// const DynaStripesCurrentNetworkExplorerUrl = 'https://rinkeby.etherscan.io/'; 
 
 // Ropsten
 // const DynaStripesCurrentNeworkID = 3; 
-// const DynaStripesCurreNetworkName = "Ropsten Testnet";
+// const DynaStripesCurrentNetworkName = "Ropsten Testnet";
 // const DynaStripesContractAddress = '0x379A3dA759A131504085E485a75cA2202fB80476'; 
-// const DynaStripesEtherscanTxLink = 'https://ropsen.etherscan.io/'; 
+// const DynaStripesCurrentNetworkExplorerUrl = 'https://ropsen.etherscan.io/'; 
 
 // Mainnet
 // const DynaStripesCurrentNeworkID = 1; 
-// const DynaStripesCurreNetworkName = "Ethereum Mainnet";
+// const DynaStripesCurrentNetworkName = "Ethereum Mainnet";
 // const DynaStripesContractAddress = '0xeD03568eaC21c1D0316c87cC09c0ce85f0000c65'; 
-// const DynaStripesEtherscanLink = 'https://www.etherscan.io/';
+// const DynaStripesCurrentNetworkExplorerUrl = 'https://www.etherscan.io/';
 
 // POLYGON
 
@@ -33,7 +35,7 @@ const DynaStripesCurrentNetworkName = "Matic Mumbai";
 const DynaStripesCurrentNetworkCurrencySymbol = "MATIC";
 const DynaStripesCurrentNetworkRpcUrl = "https://rpc-mumbai.maticvigil.com/";
 const DynaStripesCurrentNetworkExplorerUrl = "https://mumbai.polygonscan.com/";
-const DynaStripesContractAddress = '0x39B66EAabD9aA27789bBB47B135C632f17373f0D';
+const DynaStripesContractAddress = '0x6821E3753822de63D6BAfb0F0381140686fCB71c';
 
 export { DynaStripesCurrentNeworkID, 
          DynaStripesCurrentNetworkName, 
