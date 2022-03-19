748e65338288b4d9b52cbcedd71ff1a957f8b306
Richard Rauser <richard@raaza.net>
2022-03-19 11:07:33 +0000

    Shortening var names in traits to match SOL

--- src/utils/Traits.js
+++ src/utils/Traits.js
@@ -11,54 +11,54 @@ function buildTraitsText(zoom, tintRed, tintGreen, tintBlue, tintAlpha, rotation
     return speed + " " + color + " " + form;
 }
 
-function getSpeed(tintAlpha, speedMin, speedMax) { 
+function getSpeed(alpha, min, max) { 
     
     var speed = "";
 
-    if (tintAlpha > 130 && speedMin > 220) {
+    if (alpha > 130 && min > 220) {
         speed = "languid";
-    } else if (tintAlpha > 130 && speedMin > 200) {
+    } else if (alpha > 130 && min > 200) {
         speed = "pedestrian";
     } else {
-        if (speedMax <= 25 && tintAlpha < 180) {
+        if (max <= 25 && alpha < 180) {
             speed = "manic";
-        } else if (speedMin === speedMax) {
-            if (speedMax < 30) {
+        } else if (min === max) {
+            if (max < 30) {
                 speed = "blinking";
-            } else if (speedMax > 200) {
+            } else if (max > 200) {
                 speed = "throbbing";
             } else {
                 speed = "pulsing";        
             }
-        } else if (speedMax < 35) {
+        } else if (max < 35) {
             speed = "flashing";
-        } else if (speedMax < 70) {
+        } else if (max < 70) {
             speed = "flickering";
-        } else if (speedMax < 105) {
+        } else if (max < 105) {
             speed = "quivering";
-        } else if (speedMax < 150) {
+        } else if (max < 150) {
             speed = "vibrating";
-        } else if (speedMin > 200) {
+        } else if (min > 200) {
             speed = "meandering";
-        } else if (speedMin > 150) {
+        } else if (min > 150) {
             speed = "drifting";
-        } else if (speedMin > 120) {
+        } else if (min > 120) {
             speed = "flowing";
-        } else if (speedMin > 100) {
+        } else if (min > 100) {
             speed = "shifting";
-        } else if (speedMax - speedMin > 150) {
+        } else if (max - min > 150) {
             speed = "scintillating";        
         } else {
             speed = "oscillating";
         } 
 
-        if (tintAlpha > 213) {
+        if (alpha > 213) {
             speed = "gently " + speed;
-        } else if (tintAlpha > 200) {
+        } else if (alpha > 200) {
             speed = "tepidly " + speed;
-        } else if (tintAlpha < 20) {
+        } else if (alpha < 20) {
             speed = "magnificently " + speed;
-        } else if (tintAlpha < 50) {
+        } else if (alpha < 50) {
             speed = "powerfully " + speed;
         }    
     }
@@ -67,48 +67,48 @@ function getSpeed(tintAlpha, speedMin, speedMax) {
 }
 
 
-function getColor(tintRed, tintGreen, tintBlue, tintAlpha) { 
+function getColor(red, green, blue, alpha) { 
     var color = "";
 
-    console.log("Red: " + tintRed + " Green: "+ tintGreen + " Blue: " + tintBlue);
+    console.log("Red: " + red + " Green: "+ green + " Blue: " + blue);
 
-    if (tintAlpha > 127) {
+    if (alpha > 127) {
         const difference = 150;
-        if (tintAlpha > 200 && tintRed < 50 && tintGreen < 50 && tintBlue < 50) {
+        if (alpha > 200 && red < 50 && green < 50 && blue < 50) {
             color = "gloomy";
-        } else if (tintAlpha > 200 && tintRed > 200 && tintGreen > 200 && tintBlue > 200) {
+        } else if (alpha > 200 && red > 200 && green > 200 && blue > 200) {
             color = "pale";
-        } else if (tintRed > tintGreen && tintRed - tintGreen >= difference && tintRed > tintBlue && tintRed - tintBlue >= difference) {
+        } else if (red > green && red - green >= difference && red > blue && red - blue >= difference) {
             color = "red";
-        } else if (tintGreen > tintRed && tintBlue === 0) {
+        } else if (green > red && blue === 0) {
             color = "green";
-        } else if (tintGreen > tintRed && tintGreen - tintRed >= difference && tintGreen > tintBlue && tintGreen - tintBlue >= difference) {
+        } else if (green > red && green - red >= difference && green > blue && green - blue >= difference) {
             color = "green";
-        } else if (tintBlue > tintRed && tintBlue - tintRed >= difference && tintBlue > tintGreen && tintBlue - tintGreen >= difference) {
+        } else if (blue > red && blue - red >= difference && blue > green && blue - green >= difference) {
             color = "blue";
-        } else if (tintRed === 255 && tintGreen === 0 && tintBlue > 100) {
+        } else if (red === 255 && green === 0 && blue > 100) {
             color = "pink";
-        } else if (tintRed > tintGreen && tintRed - tintGreen >= difference && tintBlue > tintGreen && tintBlue - tintGreen >= difference && tintRed > 200) {
+        } else if (red > green && red - green >= difference && blue > green && blue - green >= difference && red > 200) {
             color = "purple";
-        // } else if (tintRed > tintBlue && tintRed - tintBlue >= difference && tintGreen > tintBlue && tintGreen - tintBlue >= difference) {            
-        } else if (tintRed === 255 && tintBlue === 0 && tintGreen > 200) {
+        // } else if (red > blue && red - blue >= difference && green > blue && green - blue >= difference) {            
+        } else if (red === 255 && blue === 0 && green > 200) {
             color = "yellow";
-        } else if (tintRed === 255 && tintBlue === 0) {
+        } else if (red === 255 && blue === 0) {
             color = "orange";
-        } else if (tintRed === 0 && tintGreen < 200 && tintBlue === 255) {
+        } else if (red === 0 && green < 200 && blue === 255) {
             color = "blue";
-        } else if (tintBlue > tintRed && tintBlue - tintRed >= difference && tintGreen > tintRed && tintGreen - tintRed >= difference) {
+        } else if (blue > red && blue - red >= difference && green > red && green - red >= difference) {
             color = "cyan";
         } else {
             color = "saturated";
         }
-    } else if (tintAlpha === 0) {
+    } else if (alpha === 0) {
         color = "kaleidoscopic";
-    } else if (tintAlpha < 25) {
+    } else if (alpha < 25) {
         color = "chromatic";
-    } else if (tintAlpha < 50) {
+    } else if (alpha < 50) {
         color = "vivid";
-    } else if (tintAlpha < 85) {
+    } else if (alpha < 85) {
         color = "tinged";
     } else {
         color = "tinted"; // medium tint
@@ -117,7 +117,7 @@ function getColor(tintRed, tintGreen, tintBlue, tintAlpha) {
     return color;
 }
 
-function getForm(zoom, rotationDegrees, rotationRange, stripeWidthMin, stripeWidthMax) { 
+function getForm(zoom, rotationDegrees, rotationRange, widthMin, widthMax) { 
 
     var form = "";
     rotationDegrees = rotationDegrees % 180; // because forms rotated 180 degrees are the same.
@@ -137,64 +137,64 @@ function getForm(zoom, rotationDegrees, rotationRange, stripeWidthMin, stripeWid
             form = "rotated square";
         }
     } else if (zoom < 30) {
-        if (zoom < 20 && stripeWidthMin > 70 && rotationRange < 60 && rotationRange > 10) {
+        if (zoom < 20 && widthMin > 70 && rotationRange < 60 && rotationRange > 10) {
             form = "ribbons";
-        } else if ( zoom < 20 && rotationRange < 30 && stripeWidthMin > 200) {
+        } else if ( zoom < 20 && rotationRange < 30 && widthMin > 200) {
             form = "banners";
-        } else if (zoom < 20 && stripeWidthMax < 75 && rotationRange < 60 && rotationRange > 10) {
+        } else if (zoom < 20 && widthMax < 75 && rotationRange < 60 && rotationRange > 10) {
             form = "streamers";
-        } else if (stripeWidthMax < 60 && rotationRange > 130) {
+        } else if (widthMax < 60 && rotationRange > 130) {
             form = "laser beams";
-        } else if (stripeWidthMax < 120 && rotationDegrees >= 45 && rotationDegrees < 135) {
+        } else if (widthMax < 120 && rotationDegrees >= 45 && rotationDegrees < 135) {
             form = "beams";
-        } else if (stripeWidthMax < 120 && rotationRange < 90) {
+        } else if (widthMax < 120 && rotationRange < 90) {
             form = "poles";
         } else if (rotationDegrees >= 45 && rotationDegrees < 135) {
             form = "streaks";
         } else { // rotation < 45 || rotation > 135
             form = "shafts";
         }
-    } else if (zoom > 50 && stripeWidthMax < 60 && rotationRange < 30) {
+    } else if (zoom > 50 && widthMax < 60 && rotationRange < 30) {
         form = "match sticks";
-    } else if (zoom > 50 && stripeWidthMax < 60 && rotationRange < 60) {
+    } else if (zoom > 50 && widthMax < 60 && rotationRange < 60) {
         form = "scattered match sticks";
-    } else if (zoom > 50 && stripeWidthMin > 70 && stripeWidthMax < 110 && rotationRange > 10 && rotationRange < 40) {
+    } else if (zoom > 50 && widthMin > 70 && widthMax < 110 && rotationRange > 10 && rotationRange < 40) {
         form = "twigs";
-    } else if (zoom > 50 && stripeWidthMin > 70 && stripeWidthMax < 110 && rotationRange > 10 && rotationRange < 60) {
+    } else if (zoom > 50 && widthMin > 70 && widthMax < 110 && rotationRange > 10 && rotationRange < 60) {
         form = "scattered twigs";
-    } else if (zoom > 80 &&stripeWidthMax < 60 && rotationRange > 130) {
+    } else if (zoom > 80 &&widthMax < 60 && rotationRange > 130) {
         form = "birds nest";
-    } else if ( zoom > 70 && rotationRange < 70 && stripeWidthMin > 200 && (rotationDegrees <= 25 || rotationDegrees >= 155)) {
+    } else if ( zoom > 70 && rotationRange < 70 && widthMin > 200 && (rotationDegrees <= 25 || rotationDegrees >= 155)) {
         form = "pillars";
-    } else if ( zoom > 70 && rotationRange < 70 && stripeWidthMin > 200 && (rotationDegrees <= 115 && rotationDegrees >= 65)) {
+    } else if ( zoom > 70 && rotationRange < 70 && widthMin > 200 && (rotationDegrees <= 115 && rotationDegrees >= 65)) {
         form = "bricks";
-    } else if (zoom > 55 && stripeWidthMin > 70 && rotationRange >= 5 && rotationRange <= 30 && (rotationDegrees <= 10 || rotationDegrees >= 170)) {
+    } else if (zoom > 55 && widthMin > 70 && rotationRange >= 5 && rotationRange <= 30 && (rotationDegrees <= 10 || rotationDegrees >= 170)) {
         form = "cluttered books";
-    } else if (zoom > 55 && stripeWidthMin > 70 && rotationRange <= 30 && rotationDegrees >= 80 && rotationDegrees <= 100) {
+    } else if (zoom > 55 && widthMin > 70 && rotationRange <= 30 && rotationDegrees >= 80 && rotationDegrees <= 100) {
         form = "stacked books";
-    } else if (zoom > 55 && stripeWidthMin > 70 && rotationRange < 60 && rotationDegrees >= 50 && rotationDegrees <= 130) {
+    } else if (zoom > 55 && widthMin > 70 && rotationRange < 60 && rotationDegrees >= 50 && rotationDegrees <= 130) {
         form = "tumbling books";
-    } else if (zoom > 55 && stripeWidthMin > 50 && stripeWidthMax < 150 && rotationRange < 50 && rotationDegrees >= 80 && rotationDegrees <= 100) {
+    } else if (zoom > 55 && widthMin > 50 && widthMax < 150 && rotationRange < 50 && rotationDegrees >= 80 && rotationDegrees <= 100) {
         form = "broken ladder";
-    } else if (zoom > 55 && rotationRange > 10 && rotationRange < 60 && rotationDegrees >= 50 && rotationDegrees <= 130 && stripeWidthMax - stripeWidthMin >= 150) {
+    } else if (zoom > 55 && rotationRange > 10 && rotationRange < 60 && rotationDegrees >= 50 && rotationDegrees <= 130 && widthMax - widthMin >= 150) {
         form = "collapsing building";
-    } else if (stripeWidthMin > 25 && stripeWidthMax < 200 && rotationRange <= 15) {
+    } else if (widthMin > 25 && widthMax < 200 && rotationRange <= 15) {
         form = "jitters";
-    } else if (stripeWidthMin > 25 && stripeWidthMax < 200 && rotationRange <= 45) {
+    } else if (widthMin > 25 && widthMax < 200 && rotationRange <= 45) {
         form = "wobbles";
-    } else if ( zoom > 50 && stripeWidthMin > 200) {
+    } else if ( zoom > 50 && widthMin > 200) {
         form = "blocks";
-    } else if ( zoom > 90 && stripeWidthMax - stripeWidthMin >= 150 && rotationRange > 150) {
+    } else if ( zoom > 90 && widthMax - widthMin >= 150 && rotationRange > 150) {
         form = "masterpiece";
-    } else if (rotationRange > 100 && stripeWidthMax < 150) {
+    } else if (rotationRange > 100 && widthMax < 150) {
         form = "lunacy";
-    } else if (rotationRange < 60 && stripeWidthMin > 150) {
+    } else if (rotationRange < 60 && widthMin > 150) {
         form = "tranquility";
-    } else if (stripeWidthMin > 120) {
+    } else if (widthMin > 120) {
         form = "bars";
-    } else if (stripeWidthMax < 100 && rotationRange > 60) {
+    } else if (widthMax < 100 && rotationRange > 60) {
         form = "scattered lines";
-    } else if (stripeWidthMax < 100) {
+    } else if (widthMax < 100) {
         form = "lines";
     } else if (zoom > 40 && rotationRange > 90) {
         form = "reverie";
