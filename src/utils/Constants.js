313a3a9b9176fc50b7db36b7b7e3b61342669026
Richard Rauser <richard@raaza.net>
2022-04-15 14:34:13 +0100

    contract now with base64 encoded onchain metadata... image offchain

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -42,7 +42,7 @@ function networkConfigFor(currentNetwork) {
             DynaStripesCurrentNetworkCurrencySymbolKey: "MATIC",
             DynaStripesCurrentNetworkRpcUrlKey: "https://rpc-mumbai.maticvigil.com/",
             DynaStripesCurrentNetworkExplorerUrlKey: "https://mumbai.polygonscan.com/",
-            DynaStripesContractAddressKey: '0xA513f110b32adC94bD4495E2fDf869c992cf06E3' //pre-opensea support: '0xfE5b53733fA92D335e08dAe84fC98f98Fb8BD535'
+            DynaStripesContractAddressKey: '0x30364Bb87fed7b1A80760c22e282902689831836' //pre-opensea support: '0xfE5b53733fA92D335e08dAe84fC98f98Fb8BD535'
         }
     } else if (currentNetwork === DynaStripeRinkebyNetwork) {
         return {
