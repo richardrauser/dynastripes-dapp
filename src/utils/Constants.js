92fcf57f9e9dae0bb260a7993e2a1bb4a49a426b
Richard Rauser <richard@raaza.net>
2022-04-15 08:47:15 +0100

    new contract pointing at offchain metadata, offchain image

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -42,7 +42,7 @@ function networkConfigFor(currentNetwork) {
             DynaStripesCurrentNetworkCurrencySymbolKey: "MATIC",
             DynaStripesCurrentNetworkRpcUrlKey: "https://rpc-mumbai.maticvigil.com/",
             DynaStripesCurrentNetworkExplorerUrlKey: "https://mumbai.polygonscan.com/",
-            DynaStripesContractAddressKey: '0xa22aFbD1dE2f4dc61ED7a0Dab76de5F5A8e99467' //pre-opensea support: '0xfE5b53733fA92D335e08dAe84fC98f98Fb8BD535'
+            DynaStripesContractAddressKey: '0xE99d2fAF16F7ebAa32151f76946C3Be23B702b71' //pre-opensea support: '0xfE5b53733fA92D335e08dAe84fC98f98Fb8BD535'
         }
     } else if (currentNetwork === DynaStripeRinkebyNetwork) {
         return {
